e971d505342e17da539ac7ac96be81b971543a7d
James Brown <brownj95@gmail.com>
2021-05-26 21:15:35 +0000

    Add about page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   
   def show
-    @all_products = Product.all.count
-    @all_categories = Category.all.count
+    @all_products = Product.count
+    @all_categories = Category.count
   end
 end
